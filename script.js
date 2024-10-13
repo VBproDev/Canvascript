f060d06cd79ff97b4307524c00048b03c8ffcdc4
Pappu Rohit <rohitpappu23@gmail.com>
2024-10-13 16:18:58 +0530

    Restructured functions for readability

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // @ts-check
+
+// Initalize HTML elements
 const /** @type{HTMLCanvasElement | null} */ canvas = document.querySelector('.canvas');
 const ctx = canvas ? canvas.getContext('2d') : null;
 const /** @type{HTMLButtonElement | null} */ reset = document.querySelector('.reset');
@@ -10,17 +12,10 @@ const /** @type{HTMLInputElement | null} */ design = document.querySelector('.de
 const /** @type{HTMLButtonElement | null} */ userDesign = document.querySelector('.user-design');
 const /** @type{HTMLInputElement | null} */ newDesign = document.querySelector('.new-design');
 const /** @type{HTMLButtonElement | null} */ save = document.querySelector('.save');
-const localCanvasArray = localStorage.getItem('canvasArray');
 const /** @type{HTMLInputElement | null} */ widthInput = document.querySelector('.width');
 const /** @type{HTMLInputElement | null} */ heightInput = document.querySelector('.height');
 
-const previewLineHandler = (/** @type {PointerEvent} */ e) => {
-    clear();
-    drawGrid();
-    drawLines();
-    previewLine(e);
-};
-
+// Initialize local variables
 let stroke = 1;
 let color = '#000000';
 let canvasWidth;
@@ -30,11 +25,184 @@ let y;
 let num = -1;
 let canvasArray = [];
 let warnedUser = false;
+const localCanvasArray = localStorage.getItem('canvasArray');
 
-/**
- * @param {{ offsetX: any; offsetY: any; }} event
- */
-function previewLine(event) {
+resize('def');
+drawGrid();
+if (localCanvasArray !== null) {
+    canvasArray = JSON.parse(localCanvasArray);
+    drawLines();
+}
+setArray();
+initializeEventListeners();
+
+function initializeEventListeners() {
+    window.addEventListener('resize', windowResizeHandler);
+    window.addEventListener('beforeunload', windowPreLoadHandler);
+
+    if (canvas) {
+        canvas.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+        });
+        canvas.addEventListener('pointerup', canvasPointerHandler);
+        canvas.addEventListener('pointerdown', canvasPointerHandler);
+
+    }
+
+    if (redo) {
+        redo.addEventListener('click', () => {
+            clear();
+            redoFunc();
+            drawGrid();
+            drawLines();
+            setArray();
+        });
+    }
+
+    if (generate) {
+        generate.addEventListener('click', () => {
+            generateCode();
+        });
+    }
+
+    if (reset) {
+        reset.addEventListener('click', () => {
+            clear();
+            drawGrid();
+            canvasArray = [];
+            num = -1;
+            localStorage.removeItem('canvasArray');
+            stroke = 1;
+            color = '#000000';
+            setArray();
+        });
+    }
+
+    if (set) {
+        set.addEventListener('click', () => {
+            if (widthInput && heightInput) {
+                canvasWidth = parseInt(widthInput.value);
+                canvasHeight = parseInt(heightInput.value);
+                resize('custom', canvasWidth, canvasHeight);
+                drawGrid();
+                drawLines();
+            }
+        });
+    }
+
+    if (copy) {
+        copy.addEventListener('click', () => {
+            copyText();
+        });
+    }
+
+    if (userDesign) {
+        userDesign.addEventListener('click', () => {
+            clear();
+            canvasArray = newDesign ? JSON.parse(newDesign.value) : [];
+            drawGrid();
+            drawLines();
+            setArray();
+        });
+    }
+
+    if (save) {
+        save.addEventListener('click', () => {
+            localStorage.setItem('canvasArray', JSON.stringify(canvasArray));
+        });
+    }
+}
+
+function windowResizeHandler() {
+    const canvasWidth = widthInput ? parseInt(widthInput.value) : 0;
+    const canvasHeight = heightInput ? parseInt(heightInput.value) : 0;
+
+    if (!(canvasWidth && canvasHeight)) {
+        resize('def');
+    } else {
+        resize('custom', canvasWidth, canvasHeight);
+    }
+
+    drawGrid();
+    drawLines();
+}
+
+function windowPreLoadHandler(/** @type {Event} */ e) {
+    if (localStorage.getItem('canvasArray') !== JSON.stringify(canvasArray) && !warnedUser) {
+        e.preventDefault();
+        warnedUser = true;
+    }
+}
+
+function canvasPointerHandler(/** @type {PointerEvent} */ e) {
+    if (e.type === 'pointerup') {
+        if (stroke !== 0 && canvas && ctx) {
+            let a = Math.round(e.offsetX);
+            let b = Math.round(e.offsetY);
+
+            clear();
+            drawGrid();
+            drawLines();
+
+            canvas.removeEventListener('pointermove', previewLineHandler);
+
+            num++;
+            canvasArray[num] = a;
+            num++;
+            canvasArray[num] = b;
+
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = stroke;
+            ctx.moveTo(x, y);
+            ctx.lineTo(a, b);
+            ctx.stroke();
+            setArray();
+        }
+    } else if (e.type === 'pointerdown') {
+        const /** @type{HTMLInputElement | null} */ strokeInput = document.querySelector('.stroke-width');
+        const localStroke = strokeInput ? parseInt(strokeInput.value) : 1;
+        if (localStroke !== 0 && canvas) {
+            const /** @type{HTMLInputElement | null} */ colorInput = document.querySelector('.color');
+            const localColor = colorInput ? colorInput.value : color;
+
+            if (localColor !== color) {
+                color = localColor;
+                num++;
+                canvasArray[num] = color;
+            }
+
+            if (localStroke !== stroke) {
+                stroke = localStroke;
+                num++;
+                canvasArray[num] = stroke;
+            }
+
+            x = Math.round(e.offsetX);
+            y = Math.round(e.offsetY);
+
+            num++;
+            canvasArray[num] = x;
+            num++;
+            canvasArray[num] = y;
+
+            canvas.addEventListener('pointermove', previewLineHandler);
+        }
+        else {
+            stroke = localStroke;
+        }
+    }
+}
+
+// Canvas functions
+function previewLineHandler(/** @type {PointerEvent} */ e) {
+    clear();
+    drawGrid();
+    drawLines();
+    previewLine(e);
+};
+
+function previewLine(/** @type {PointerEvent} */ event) {
     const atX = event.offsetX;
     const atY = event.offsetY;
     if (ctx) {
@@ -45,38 +213,6 @@ function previewLine(event) {
         ctx.lineTo(atX, atY);
         ctx.stroke();
     }
-}
-
-/**
- * @param {any} n
- */
-function int(n) {
-    return typeof n === 'number';
-}
-
-function copyText() {
-    if (design) {
-        design.select();
-        design.setSelectionRange(0, 99999999);
-        navigator.clipboard.writeText(JSON.stringify(canvasArray));
-    }
-}
-
-function setArray() {
-    if (design) {
-        if (canvasArray.length === 0) {
-            design.value = '[ ]'
-        } else {
-            design.value = JSON.stringify(canvasArray);
-        };
-    }
-}
-
-function redoFunc() {
-    for (let i = 0; i < 4; i++) {
-        canvasArray.pop();
-    }
-    num -= 4;
 }
 
 function clear() {
@@ -96,12 +232,12 @@ function drawLines() {
             let CA1 = canvasArray[i];
             let CA2 = canvasArray[i + 1];
 
-            if (int(CA1) && int(CA2)) {
+            if (_int(CA1) && _int(CA2)) {
                 ctx.moveTo(CA1, CA2);
                 ctx.lineTo(canvasArray[i + 2], canvasArray[i + 3]);
                 i += 4;
             } else {
-                if (!(int(CA1) || int(CA2))) {
+                if (!(_int(CA1) || _int(CA2))) {
                     ctx.stroke();
                     ctx.beginPath();
                     ctx.strokeStyle = CA1;
@@ -123,46 +259,6 @@ function drawLines() {
             };
         };
         ctx.stroke();
-    }
-};
-
-function generateCode() {
-    const space = document.querySelector('.space');
-    const range = document.createRange();
-    const selection = window.getSelection();
-    let i = 0;
-    if (space && selection) {
-        space.innerHTML = `<div>const canvas = document.querySelector(\'canvas\');</div><div>const ctx = canvas.getContext(\'2d\');</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '#000000';</div><div>ctx.lineWidth = 1;</div>`
-
-
-        while (i < canvasArray.length) {
-            let CA1 = canvasArray[i];
-            let CA2 = canvasArray[i + 1];
-
-            if (int(CA1) && int(CA2)) {
-                space.innerHTML += `<div>ctx.moveTo(${CA1}, ${CA2});</div>`;
-                space.innerHTML += `<div>ctx.lineTo(${canvasArray[i + 2]}, ${canvasArray[i + 3]});</div>`;
-                i += 4;
-            } else {
-                if (!(int(CA1) || int(CA2))) {
-                    space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '${CA1}';</div>`;
-                    space.innerHTML += `<div>ctx.lineWidth = '${CA2}';</div>`;
-                    i += 2;
-                } else {
-                    if (Math.sign(CA1)) {
-                        space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.lineWidth = '${CA1}';</div>`;
-                        i++;
-                    } else {
-                        space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '${CA1}';</div>`;
-                        i++;
-                    };
-                };
-            };
-        };
-        space.innerHTML += '<div>ctx.stroke();</div>';
-        selection.removeAllRanges();
-        range.selectNodeContents(space);
-        selection.addRange(range);
     }
 };
 
@@ -209,163 +305,73 @@ function drawGrid() {
 
 }
 
-resize('def');
-drawGrid();
-
-if (localCanvasArray !== null) {
-    canvasArray = JSON.parse(localCanvasArray);
-    drawLines();
-}
-
-setArray();
-
-window.addEventListener('resize', (e) => {
-    const canvasWidth = widthInput ? parseInt(widthInput.value) : 0;
-    const canvasHeight = heightInput ? parseInt(heightInput.value) : 0;
-
-    if (!(canvasWidth && canvasHeight)) {
-        resize('def');
-    } else {
-        resize('custom', canvasWidth, canvasHeight);
+function copyText() {
+    if (design) {
+        design.select();
+        design.setSelectionRange(0, 99999999);
+        navigator.clipboard.writeText(JSON.stringify(canvasArray));
     }
+}
 
-    drawGrid();
-    drawLines();
-});
-
-window.addEventListener('beforeunload', (e) => {
-    if (localStorage.getItem('canvasArray') !== JSON.stringify(canvasArray) && !warnedUser) {
-        e.preventDefault();
-        warnedUser = true;
+function setArray() {
+    if (design) {
+        if (canvasArray.length === 0) {
+            design.value = '[ ]'
+        } else {
+            design.value = JSON.stringify(canvasArray);
+        };
     }
-});
-
-if (canvas) {
-    canvas.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-    });
-
-    canvas.addEventListener('pointerdown', (e) => {
-        const /** @type{HTMLInputElement | null} */ strokeInput = document.querySelector('.stroke-width');
-        const localStroke = strokeInput ? parseInt(strokeInput.value) : 1;
-        if (localStroke !== 0) {
-            const /** @type{HTMLInputElement | null} */ colorInput = document.querySelector('.color');
-            const localColor = colorInput ? colorInput.value : color;
-
-            if (localColor !== color) {
-                color = localColor;
-                num++;
-                canvasArray[num] = color;
-            }
-
-            if (localStroke !== stroke) {
-                stroke = localStroke;
-                num++;
-                canvasArray[num] = stroke;
-            }
-
-            x = Math.round(e.offsetX);
-            y = Math.round(e.offsetY);
-
-            num++;
-            canvasArray[num] = x;
-            num++;
-            canvasArray[num] = y;
-
-            canvas.addEventListener('pointermove', previewLineHandler);
-        }
-
-        else {
-            stroke = localStroke;
-        }
-    });
-
-    canvas.addEventListener('pointerup', (e) => {
-        if (stroke !== 0 && ctx) {
-            let a = Math.round(e.offsetX);
-            let b = Math.round(e.offsetY);
-
-            clear();
-            drawGrid();
-            drawLines();
-
-            canvas.removeEventListener('pointermove', previewLineHandler);
-
-            num++;
-            canvasArray[num] = a;
-            num++;
-            canvasArray[num] = b;
-
-            ctx.beginPath();
-            ctx.strokeStyle = color;
-            ctx.lineWidth = stroke;
-            ctx.moveTo(x, y);
-            ctx.lineTo(a, b);
-            ctx.stroke();
-            setArray();
-        }
-    });
 }
 
-if (redo) {
-    redo.addEventListener('click', () => {
-        clear();
-        redoFunc();
-        drawGrid();
-        drawLines();
-        setArray();
-    });
+function redoFunc() {
+    for (let i = 0; i < 4; i++) {
+        canvasArray.pop();
+    }
+    num -= 4;
 }
 
-if (generate) {
-    generate.addEventListener('click', () => {
-        generateCode();
-    });
-}
 
-if (reset) {
-    reset.addEventListener('click', () => {
-        clear();
-        drawGrid();
-        canvasArray = [];
-        num = -1;
-        localStorage.removeItem('canvasArray');
-        stroke = 1;
-        color = '#000000';
-        setArray();
-    });
-}
 
-if (set) {
-    set.addEventListener('click', () => {
-        if (widthInput && heightInput) {
-            canvasWidth = parseInt(widthInput.value);
-            canvasHeight = parseInt(heightInput.value);
-            resize('custom', canvasWidth, canvasHeight);
-            drawGrid();
-            drawLines();
-        }
-    });
-}
+function generateCode() {
+    const space = document.querySelector('.space');
+    const range = document.createRange();
+    const selection = window.getSelection();
+    let i = 0;
+    if (space && selection) {
+        space.innerHTML = `<div>const canvas = document.querySelector(\'canvas\');</div><div>const ctx = canvas.getContext(\'2d\');</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '#000000';</div><div>ctx.lineWidth = 1;</div>`
 
-if (copy) {
-    copy.addEventListener('click', () => {
-        copyText();
-    });
-}
 
-if (userDesign) {
-    userDesign.addEventListener('click', () => {
-        clear();
-        canvasArray = newDesign ? JSON.parse(newDesign.value) : [];
-        drawGrid();
-        drawLines();
-        setArray();
-    });
-}
+        while (i < canvasArray.length) {
+            let CA1 = canvasArray[i];
+            let CA2 = canvasArray[i + 1];
 
-if (save) {
-    save.addEventListener('click', () => {
-        localStorage.setItem('canvasArray', JSON.stringify(canvasArray));
-    });
+            if (_int(CA1) && _int(CA2)) {
+                space.innerHTML += `<div>ctx.moveTo(${CA1}, ${CA2});</div>`;
+                space.innerHTML += `<div>ctx.lineTo(${canvasArray[i + 2]}, ${canvasArray[i + 3]});</div>`;
+                i += 4;
+            } else {
+                if (!(_int(CA1) || _int(CA2))) {
+                    space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '${CA1}';</div>`;
+                    space.innerHTML += `<div>ctx.lineWidth = '${CA2}';</div>`;
+                    i += 2;
+                } else {
+                    if (Math.sign(CA1)) {
+                        space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.lineWidth = '${CA1}';</div>`;
+                        i++;
+                    } else {
+                        space.innerHTML += `<div>ctx.stroke();</div><div>ctx.beginPath();</div><div>ctx.strokeStyle = '${CA1}';</div>`;
+                        i++;
+                    };
+                };
+            };
+        };
+        space.innerHTML += '<div>ctx.stroke();</div>';
+        selection.removeAllRanges();
+        range.selectNodeContents(space);
+        selection.addRange(range);
+    }
+};
+
+function _int(/** @type {any} */ n) {
+    return typeof n === 'number';
 }
